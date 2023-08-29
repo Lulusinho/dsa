0255efd9134636dc035a4cf3933d5af22690f8f7
unknown <roberto4luis1@gmail.com>
2023-08-29 09:19:06 -0300

    .

--- rbtree/main.c
+++ rbtree/main.c
@@ -7,13 +7,27 @@ int main(int argc, char **argv)
     printf("\n");
     tree = rb_insert(tree, 30);
     preorder(tree);
-printf("\n");
+    printf("\n");
     tree = rb_insert(tree, 5);
     preorder(tree);
-printf("\n");
+    printf("\n");
     tree = rb_insert(tree, 15);
     preorder(tree);
     printf("\n");
+    tree = rb_insert(tree, 60);
+    preorder(tree);
+    printf("\n");
+    tree = rb_insert(tree, 70);
+    preorder(tree);
+    printf("\n");
+    tree = rb_insert(tree, 1);
+    preorder(tree);
+    printf("\n");
+    tree = rb_insert(tree, 6);
+    preorder(tree);
+    printf("\n");
+
+    printf("%d", get_color(tree));
 
     return 0;
 }
